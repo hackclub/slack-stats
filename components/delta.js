9c5467f2d433a238f856e25ce4c219ae185f4f27
Lexi Mattick <lexi.mattick@gmail.com>
2023-04-03 12:22:01 -0400

    Format infinity correctly

--- components/delta.js
+++ components/delta.js
@@ -3,7 +3,7 @@ import { IoCaretUp, IoCaretDown, IoCaretForward } from 'react-icons/io5'
 export default function Delta({ a, b, iconSide }) {
 	const delta = ((b - a) / a) * 100
 
-	const perc = <div>{Math.abs(Math.round(delta))}%</div>
+	const perc = <div>{delta === Infinity ? 'Inf' : Math.abs(Math.round(delta))}%</div>
 	const icon = delta === 0
 		? <IoCaretForward />
 		: delta >= 0 ? <IoCaretUp /> : <IoCaretDown />
