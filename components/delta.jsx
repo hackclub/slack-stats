d396c61230bca0e8effbccd5cbea10c58e2be9e2
Lexi Mattick <lexi.mattick@gmail.com>
2023-04-04 12:39:09 -0400

    JS->JSX and properly format <0.5 deltas

--- components/delta.jsx
+++ components/delta.jsx
@@ -3,7 +3,13 @@ import { IoCaretUp, IoCaretDown, IoCaretForward } from 'react-icons/io5'
 export default function Delta({ a, b, iconSide }) {
 	const delta = ((b - a) / a) * 100
 
-	const perc = <div>{delta === Infinity ? 'Inf' : Math.abs(Math.round(delta))}%</div>
+	const perc = <div>
+		{delta === Infinity
+			? 'Inf'
+			: delta < 0.5 && delta > 0
+				? Math.abs(Math.ceil(delta * 10) / 10)
+				: Math.abs(Math.round(delta))}%
+	</div>
 	const icon = delta === 0
 		? <IoCaretForward />
 		: delta >= 0 ? <IoCaretUp /> : <IoCaretDown />
